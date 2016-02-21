17e2bca644df0c90ee628221e76403361cbfcbd6
CYRIL <CYRIL@CYRIL-PC>
2016-02-21 15:29:13 -0500

    Added testGetBalanceIs100AfterDeposit500ThenWithdraw400() testGetBalanceIsZeroAfterDeposit500ThenClose()

--- test/model/AccountTest.java
+++ test/model/AccountTest.java
@@ -44,14 +44,31 @@ public class AccountTest {
         double result = instance.getBalance();
         assertEquals(expResult, result, 0.0);
     }
-
+    /**
+ * Cyril Dsouza
+ */
     @Test
     public void testGetBalanceIs100AfterDeposit500ThenWithdraw400() {
-        
+        double cash = 500.0;
+        double withdraw = 400.0;
+        Account instance = new Account();
+        instance.deposit(cash);
+        instance.withdraw(withdraw);
+        double expResult = 100.0;
+        double result = instance.getBalance();
+        assertEquals(expResult, result,0.0);
     }
-    
+    /**
+ * Cyril Dsouza
+ */
     @Test
     public void testGetBalanceIsZeroAfterDeposit500ThenClose() {
-        
+        double cash = 500.0;
+        Account instance = new Account();
+        instance.deposit(cash);
+        instance.close();
+        double expResult = 0;
+        double result = instance.getBalance();
+        assertEquals(expResult, result,0.0);
     }
 }
